ab748e38f69d3ab2214fb5678b4031510894ee2b
Nicholas Gulrajani <nichgul@LT-APL0075987.local>
2021-05-21 07:34:37 -0400

    WIP

--- backend.sh
+++ backend.sh
@@ -1,6 +1,6 @@
 backend "azurerm" {
-    resource_group_name   = "jonnychipz-infra"
-    storage_account_name  = "jonnychipztstate"
-    container_name        = "tstate"
-    key                   = "77Q4LUB5o9wRdbPYDt+0kGZP+L8Sj9E/FNXg7lZBQS5z3mLod5cyan4wA19CR1SmlqIRUFQfhuQrPVaGzNhjGw=="
+    resource_group_name   = "nick-dev-terraform-backend-rg"
+    storage_account_name  = "nicdevterraformbackendsa"
+    container_name        = "nick-terraform-backend-files"
+    key                   =  "z+upNiAp4jOfS0YXzoeO2cnqc5mw+zK+7eP09H0C7rDsASU4+NzWRXP6sdoH7ZdfSIeoemHGTkrMuqhgsRUM1Q=="
 }

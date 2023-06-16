740382ea5339c5d9b4448d90cc6acb60c0ab893d
Nicholas Gulrajani <nicholas.gulrajani@cdw.com>
2023-06-16 09:59:28 -0400

    WIP

--- backend.sh
+++ backend.sh
@@ -2,5 +2,5 @@ backend "azurerm" {
     resource_group_name   = "nick-dev-terraform-backend-rg"
     storage_account_name  = "nicdevterraformbackendsa"
     container_name        = "nick-terraform-backend-files"
-    key                   =  "z+upNiAp4jOfS0YXzoeO2cnqc5mw+zK+7eP09H0C7rDsASU4+NzWRXP6sdoH7ZdfSIeoemHGTkrMuqhgsRUM1Q=="
+    key                   =  "BnV9Mp79R+xPvWMytRtWBL/lrI1TGNxQlpXzYqq1i6J+g4kb4fr0j7ZqdVCsj4pL3yZLgKO48H2y+AStixAx/w=="
 }
